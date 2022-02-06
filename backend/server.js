bd9d41eb55a7a070d6133aa96eb5b697f1f1d1d6
garrettdurbin <garrettdurbin@gmail.com>
2022-02-06 16:38:34 -0600

    Error and Exception Handling

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 // Import Express Module
 const express = require('express')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 5000
 
 // Create an Express Application
 const app = express()
+
+// Send Res in JSON
+app.use(express.json())
+// Receive Body as URL Encoded
+app.use(express.urlencoded({extended: false}))
 
 // Route Definition
 app.get('/', (req, res) => {
@@ -14,6 +20,8 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 // Start Server, Print Port
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
