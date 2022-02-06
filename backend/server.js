11dd5c2a0cd5bcdca86dc902c7ef28696227fd24
garrettdurbin <garrettdurbin@gmail.com>
2022-02-06 15:29:51 -0600

    Added Routes and Controller

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
+// Import Express Module
 const express = require('express')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 5000
 
+// Create an Express Application
 const app = express()
 
+// Route Definition
 app.get('/', (req, res) => {
   res.status(200).json({message: 'Welcome to the Support Desk API'})
 })
 
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+// Start Server, Print Port
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 
 
