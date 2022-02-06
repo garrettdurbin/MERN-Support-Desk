5017ba48f062883accc3248a161e32316c39041b
garrettdurbin <garrettdurbin@gmail.com>
2022-02-06 16:56:50 -0600

    Connect to Database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 // Import Express Module
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
+
+// Connect to Database
+connectDB()
 
 // Create an Express Application
 const app = express()
